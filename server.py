68b26f39904204a13b3096a2f16780f296993d03
Marnelli Jabonero <Marnelli Jabonero>
2015-11-06 15:20:48 -0800

    functioning but does not append log

--- server.py
+++ server.py
@@ -5,8 +5,43 @@ app.secret_key='steppinondabeach'
 
 @app.route('/')
 def index():
+	try:
+		session['foundgold']
+	except:
+		session['foundgold'] = 0
 	return render_template('index.html')
 
-@app.route('/process_money')
+@app.route('/process_money', methods=['POST'])
 def money():
+	if request.form['building'] == 'farm':
+		session['gold'] = random.randrange(10,21)
+	elif request.form['building'] == 'cave':
+		session['gold'] = random.randrange(5,11)
+	elif request.form['building'] == 'house':
+		session['gold'] = random.randrange(2,6)
+	elif request.form['building'] == 'HIGHROLLERYO':
+		luck = random.randrange(1,100)
+		session['gamble'] = random.randrange(0,51)
+		session['gold'] = session['gamble']
+	
+	if request.form['building'] == 'HIGHROLLERYO':
+		if luck < 50:
+			session['foundgold'] -= session['gamble'] # IF LOST
+			session['message'] = 'Lost gold!'
+			
+		if luck >= 50:
+			session['foundgold'] += session['gamble'] # IF GAINED
+			session['message'] = 'Found gold!'
+	else:		
+		session['foundgold'] += session['gold']
+		session['message'] = 'Found gold!'
+
 	return redirect('/')
+
+@app.route('/reset', methods=['POST'])
+def reset():
+	session['foundgold'] = 0
+	return redirect('/')
+
+app.run(debug=True)
+
